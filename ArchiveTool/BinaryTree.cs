906bf18a0b560984c411575ed3eb1493f2c6f961
Sergiy Lichenko <sergiy.lichenko@outlook.com>
2017-05-05 15:23:35 +0300

    Update BinaryTree.cs

--- ArchiveTool/BinaryTree.cs
+++ ArchiveTool/BinaryTree.cs
@@ -8,26 +8,26 @@ namespace ArchiveTool
 {
     public class Node
     {
-        public int Data { get;  set; }//код символа
-        public int Weight { get;  set; }//количество повторений
-        public Node Left { get; set; }//левая ветка дерева
-        public Node Right { get; set; }//правая ветка дерева
+        public int Data { get;  set; }
+        public int Weight { get;  set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
 
 
-        public Node() { }//конструктор по умолчанию
-        public Node(byte data, int weight)//перегруженый конструктор
+        public Node() { }
+        public Node(byte data, int weight)
         {
-            this.Data = data;       //установка полей
-            this.Weight = weight;   //
+            this.Data = data;       
+            this.Weight = weight;
         }
         
-        public Node Add(Node left, Node right)//объдинение двух узлов
+        public Node Add(Node left, Node right)
         {
-            this.Weight = left.Weight + right.Weight;//
-            this.Left = left;                        //установка полей
-            this.Right = right;                      //
+            this.Weight = left.Weight + right.Weight;
+            this.Left = left;                      
+            this.Right = right;                    
 
-            return this;//возвращение текущего узла
+            return this;
         }
     }
 }
