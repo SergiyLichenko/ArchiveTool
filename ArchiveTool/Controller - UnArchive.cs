61283763518fedb6fc0f84f558ad37aff5f5fb02
Sergiy Lichenko <sergiy.lichenko@outlook.com>
2017-05-05 15:19:54 +0300

    Update binary files

--- ArchiveTool/Controller - UnArchive.cs
+++ ArchiveTool/Controller - UnArchive.cs
@@ -8,19 +8,18 @@ using System.Threading.Tasks;
 
 namespace ArchiveTool
 {
-    delegate void UnArchived(long inputSize, long outpuSize);         //делегат для пользовательского события
+    delegate void UnArchived(long inputSize, long outpuSize);         
     partial class Controller
-    {
-          
-        StringBuilder result;                           //строка результата
-        Node rootUn;
-        int tempSizeTree = -1;
+    {          
+        public event UnArchived unArchived;   
         public int unArchPersentage;
-
-        string pathUnFrom;
-        string pathUnTo;
-        public event UnArchived unArchived;             //пользовательское событие завершения разархивации
-        BackgroundWorker workerUnStatistics;
+        
+        private StringBuilder result;                          
+        private Node rootUn;
+        private int tempSizeTree = -1;
+        private string pathUnFrom;
+        private string pathUnTo;
+        private BackgroundWorker workerUnStatistics;
 
         internal void CreateUnStatistics(string fileName)
         {
@@ -39,23 +38,19 @@ namespace ArchiveTool
             model.CreateUnCounts((string)e.Argument, (BackgroundWorker)sender);
         }
 
-        internal void UnArchive(string fileNameFrom, string fileNameTo)        //разархивация
+        internal void UnArchive(string fileNameFrom, string fileNameTo)     
         {
             pathUnFrom = fileNameFrom;
             pathUnTo = fileNameTo;
-            workerForUnArchive.RunWorkerAsync(fileNameFrom);        //запуск разархивации в фоновом потоке
+            workerForUnArchive.RunWorkerAsync(fileNameFrom);       
         }
-
-
-        
-
         private void WorkerForUnArchive_DoWork(object sender, DoWorkEventArgs e)
         {
             rootUn = new Node();
             rootUn.Data = -1;
             CreateTree(rootUn);
             model.unArchPers += Model_unArchPers;
-            model.UseTree(rootUn, pathUnFrom,pathUnTo,k,(BackgroundWorker) sender);                  //использование дерева
+            model.UseTree(rootUn, pathUnFrom,pathUnTo,k,(BackgroundWorker) sender);                 
             model.unArchPers -= Model_unArchPers;
         }
 
@@ -69,45 +64,43 @@ namespace ArchiveTool
 
         private void WorkerForUnArchive_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {            
-            unArchived(new FileInfo(pathUnFrom).Length, new FileInfo(pathUnTo).Length);                          //вызов события завершения разархивации
+            unArchived(new FileInfo(pathUnFrom).Length, new FileInfo(pathUnTo).Length);                        
         }
         
         int k=-1;
-        private void CreateTree(Node rootUnarchive)         //разархивирование дерева
+        private void CreateTree(Node rootUnarchive)       
         {
             k++;
             if (tempSizeTree == model.sizeOftree)
                 return;
 
-            if (model.tempStr[k] == '1')                 //пока можна идти по дереву
+            if (model.tempStr[k] == '1')                
             {
-                if (rootUnarchive.Left == null)         //идем влево
+                if (rootUnarchive.Left == null)       
                 {
                     rootUnarchive.Left = new Node();
                     rootUnarchive.Left.Data = -1;
-                    CreateTree(rootUnarchive.Left);     //рекурсивно вызываем
+                    CreateTree(rootUnarchive.Left);    
                 }
-                else                                    //идем вправо
+                else                                   
                 {
                     rootUnarchive.Right = new Node();
                     rootUnarchive.Right.Data = -1;
                     CreateTree(rootUnarchive.Right);
                 }
                 if (rootUnarchive.Left == null || rootUnarchive.Right == null)
-                    CreateTree(rootUnarchive);              //возвращение на один уровень на верх 
+                    CreateTree(rootUnarchive);            
 
             }
-            else        //создание узла дерева
+            else      
             {
                 tempSizeTree++;
-                k++;                    //
-                string byteCode = model.tempStr.ToString().Substring(k, 8);     //строка кода символа дерева, состоящая из 0, 1
-                rootUnarchive.Data = Convert.ToByte(byteCode, 2);   //запись символа в лист дерева
+                k++;                  
+                string byteCode = model.tempStr.ToString().Substring(k, 8);    
+                rootUnarchive.Data = Convert.ToByte(byteCode, 2);  
 
                 k += 7;
             }
         }
-        
-
     }
 }
